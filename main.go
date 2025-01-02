e9b9839a8cb3a1305ab6263b7d0426e72980cc34
Joe Reid <joe@joereid.com>
2025-01-02 12:14:02 -0500

    The actual program content

--- main.go
+++ main.go
@@ -1,4 +1,55 @@
+// A simple Quake 2 master server implementation.
+//
+// Quake 2 game servers with the `public` cvar set to 1 will report to any
+// configured master servers. This allows players to find servers and see
+// basic data about each server (current map, some settings, players, scores
+// pings, etc). Game servers can specify a particular master server with a
+// command like: `setmaster <x.x.x.x[:yyyyy]>`. Port UDP/27000 is the default.
+//
+// This implemtation includes an optional HTTP server to handle API requests
+// for querying the current state/data. Enable with the `-api` flag.
 package main
 
-func main() {
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+	"time"
 
+	"github.com/packetflinger/libq2/master"
+)
+
+var (
+	listenPort = flag.Int("port", 27900, "Port to listen on")
+	listenIP   = flag.String("addr", "[::]", "IP address to listen on")
+	foreground = flag.Bool("fg", false, "Log to stdout instead of file")
+	logfile    = flag.String("logfile", "master.log", "The filename to use for the log")
+	api        = flag.Bool("api", false, "Whether or not to enable the web API")
+	apiPort    = flag.Int("apiport", 3333, "TCP port for web requests")
+	apiIP      = flag.String("apiaddr", "[::]", "The IP address to listen on for web requests")
+)
+
+func main() {
+	flag.Parse()
+	if !*foreground {
+		fp, err := os.OpenFile(*logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fp.Close()
+		log.SetOutput(io.Writer(fp))
+	}
+
+	log.Println("*** Quake 2 Master Server ***")
+	log.Printf("  (c) 2022-%d Packetflinger Industries\n", time.Now().Year())
+	log.Println("  https://github.com/packetflinger/q2master")
+
+	m := master.NewMaster()
+	m.Address = *listenIP
+	m.Port = *listenPort
+	m.ApiEnabled = *api
+	m.ApiIP = *apiIP
+	m.ApiPort = *apiPort
+	m.Run()
+}
