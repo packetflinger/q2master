5de0d8145e5f708db2051bb2d0780e09b914f0fa
Joe Reid <joe@joereid.com>
2025-01-03 15:43:20 -0500

    Add config file support

--- main.go
+++ main.go
@@ -12,12 +12,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path"
 	"time"
 
 	"github.com/packetflinger/libq2/master"
+	"google.golang.org/protobuf/encoding/prototext"
+
+	pb "github.com/packetflinger/q2master/proto"
 )
 
 var (
@@ -28,10 +33,17 @@ var (
 	api        = flag.Bool("api", false, "Whether or not to enable the web API")
 	apiPort    = flag.Int("apiport", 3333, "TCP port for web requests")
 	apiIP      = flag.String("apiaddr", "[::]", "The IP address to listen on for web requests")
+	config     = flag.String("config", ".config/q2master/config", "")
 )
 
 func main() {
 	flag.Parse()
+	c, err := loadConfig(*config)
+	applyConfig(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if !*foreground {
 		fp, err := os.OpenFile(*logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
@@ -52,4 +64,52 @@ func main() {
 	m.ApiIP = *apiIP
 	m.ApiPort = *apiPort
 	m.Run()
+}
+
+func loadConfig(f string) (*pb.MasterConfig, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("loadConfig userhomedir: %v", err)
+	}
+	data, err := os.ReadFile(path.Join(home, f))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		} else {
+			return nil, fmt.Errorf("loadConfig readfile: %v", err)
+		}
+	}
+	var config pb.MasterConfig
+	err = prototext.Unmarshal(data, &config)
+	if err != nil {
+		return nil, fmt.Errorf("loadConfig unmarshal: %v", err)
+	}
+	return &config, nil
+}
+
+func applyConfig(c *pb.MasterConfig) {
+	if c == nil {
+		return
+	}
+	if c.GetPort() != 0 {
+		*listenPort = int(c.GetPort())
+	}
+	if c.GetAddr() != "" {
+		*listenIP = c.GetAddr()
+	}
+	if c.GetForeground() {
+		*foreground = c.GetForeground()
+	}
+	if c.GetLogfile() != "" {
+		*logfile = c.GetLogfile()
+	}
+	if c.GetApi() {
+		*api = c.GetApi()
+	}
+	if c.GetApiPort() != 0 {
+		*apiPort = int(c.GetApiPort())
+	}
+	if c.GetApiAddr() != "" {
+		*apiIP = c.GetApiAddr()
+	}
 }
